076cb352429b9ad900f0de4fe019aa9d641dc2ae
corey drew bruce <1458906-coreybruce@users.noreply.gitlab.com>
2024-04-15 17:29:07 +1000

    Netlify 1.0.1-1

---
+++ README.md
@@ -0,0 +1,19 @@
+![picture](https://i.imgur.com/H4IaVKO.png)
+
+Unofficial Syncthing desktop application, Open source and multi-platform for all platforms to use. mananage your syncthing devices, connections and syncs all in the desktop app.
+
+&nbsp;&nbsp;&nbsp;&nbsp;
+
+![picture](https://i.imgur.com/wZTYNkY.png)
+
+&nbsp;&nbsp;&nbsp;&nbsp;
+
+  You can install Syncthing Desktop from the AUR for Arch/Manjaro distros.
+ [Click Here](https://aur.archlinux.org/packages/syncthingdesktop)
+
+ ### Download For All platforms (Linux, Mac OS and Windows)
+  
+  [Click to get the latest release](https://gitlab.com/syncthingdesktop/application/-/releases)
+
+ ### Author
+  * Corey Bruce
---
+++ main.js
@@ -0,0 +1,69 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, shell} = require('electron')
+const contextMenu = require('electron-context-menu');
+const path = require('path')
+app.commandLine.appendSwitch('enable-tcp-fastopen')
+app.commandLine.appendSwitch('enable-features', 'VaapiVideoDecoder')
+
+contextMenu({
+	prepend: (defaultActions, params, browserWindow, dictionarySuggestions) => [
+		{
+			label: 'Search Google for “{selection}”',
+			// Only show it when right-clicking text
+			visible: params.selectionText.trim().length > 0,
+			click: () => {
+				shell.openExternal(`https://google.com/search?q=${encodeURIComponent(params.selectionText)}`);
+			}
+		}
+	]
+});
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 750,
+    icon: __dirname + '/icon.png',
+    backgroundColor: '#2C2C2C',
+    webPreferences: {
+     contextIsolation: true,
+     spellcheck: true,
+     preload: path.join(app.getAppPath(), 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('splash.html')
+  mainWindow.maximize() // start maximized
+  mainWindow.setMenuBarVisibility(false)
+  mainWindow.setMenu(null)
+  mainWindow.show();
+  mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
